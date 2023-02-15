14a58e75c592f1f29a553cd41e1ca6609d2b321e
muktha <muktha.1689@gmail.com>
2023-02-14 20:41:39 -0800

    added javascript codes for questions

--- js/script.js
+++ js/script.js
@@ -1,23 +1,111 @@
-// STEP 1
+// STEP 1 : Camel case notation
+let someMonth;
+function theMonth(){}
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+
 // STEP 2
-// STEP 3
-// STEP 4
-// STEP 5
-// STEP 6
-// STEP 7
+// 10          // numeric literal expression
+// 'Welcome!'  // string literal expression
+// true        // boolean literal expression
+// null        // null literal expression
+
+// STEP 3 : complex/variable expression
+// let sum = 3 + 2 + 5 * 2;
+// let total = 12 + 3 * (10 / 5);
+
+// STEP 4 : declare variables but dont assign
+// let strFirstName, strLastName, strAddress, strCity, strState, strZipCode, numYourAge, strReferralSource, blnMayWeContactYou;
+
+// STEP 5 : Declaring and assigning
+// let strFirstName, strLastName, numYourAge;
+// strFirstName = 'Bob';
+// strLastName = 'Ross';
+// numYourAge = 40;
+// let strReferralSource;
+// strReferralSource = 'email';
+// let blnMayWeContactYou = true;
+// let strCity = 'San Diego', strState = 'CA', strZipCode = '92130';
+
+// STEP 6 
+// let count;
+// count = 'Number of chocolates ' + 10;
+// console.log(count);
+
+// STEP 7 
+// let contact = 'May we contact you? ' + true ;
+// console.log(contact);
+// let num = 10 + false;
+// console.log(num);
+
 // STEP 8
-// STEP 9
+// let someString = `Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."`
+// console.log(someString);
+
+// STEP 9: null and undefined
+// let value = null;
+// console.log(value);
+// let address;
+// console.log(address);
+
 // STEP 10
+// console.log(typeof('hello'));                   //string type
+// console.log(typeof(25));                        //number type
+// console.log(typeof(true));                      //boolean type
+// let animal = {type: 'Dog', color: 'Black'};     
+// console.log(typeof(animal));                    //object type
+// let word;
+// console.log(typeof(word));                      //undefined type
+
 // STEP 11
+// alert('Hello ' + 'Muktha' + ', welcome to JavaScript class!');
+
 // STEP 12
+// let myName = 'Muktha';
+// alert('Hello ' + myName + ', welcome to JavaScript class!');
+
 // STEP 13
+// let course = 'JavaScript';
+// let myName = 'Muktha';
+// alert('Hello ' + myName + ', welcome to ' + course + ' class!');
+
 // STEP 14
+// let course = 'JavaScript';
+// let myName = 'Muktha';
+// alert('Hello ' + myName + '.\nWelcome to ' + course + ' class!');
+
 // STEP 15
+// let course = 'JavaScript';
+// let myName = prompt('what is your name?');
+// alert('Hello ' + myName + '.\nWelcome to ' + course + ' class!');
+
 // STEP 16
+// let myName = prompt('what is your name?');
+// let course = prompt('what class are you taking?');
+// alert('Hello ' + myName + '.\nWelcome to ' + course + ' class!');
+
 // STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
 // STEP 18
-// STEP 18
+// let x = 20;
+// console.log(x += 20);
+
 // STEP 19
+// let x = 20;
+// console.log(x *= 5);
+
 // STEP 20
+// let x = 20 % 3;
+// console.log(x /= 1);
+
 // STEP 21
+// let x = 1, y = 5;
+// console.log((x < 10 && y < 10));    //evaluates to true
+
 // STEP 22
+// let greet = 'Hello', a = 6;
+// console.log((greet == 'welcome' || a > 10));        //evaluates to false
